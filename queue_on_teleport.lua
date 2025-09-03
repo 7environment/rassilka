85461a2e22f0dd4070b7fd03d8b463842005737c
7environment <artemmartynenko485@gmail.com>
2025-09-03 20:17:04 +1000

    Update queue_on_teleport.lua

--- queue_on_teleport.lua
+++ queue_on_teleport.lua
@@ -3,5 +3,5 @@ local TextChatService = game:GetService("TextChatService")
 local channel = TextChatService.TextChannels["RBXGeneral"]
 channel:SendAsync("Привет, хорошей игры, пока")
 task.wait(3)
-queue_on_teleport(loadsrting(game:HttpGet("https://github.com/7environment/rassilka/blob/main/queue_on_teleport.lua", true)))
+queue_on_teleport(game:HttpGet("https://github.com/7environment/rassilka/blob/main/queue_on_teleport.lua", true))
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
